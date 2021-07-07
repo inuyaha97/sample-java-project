221b3a6a259876623e3550a2722ba8a0d5168e55
AjaySarathy <ajaysarathy@saryam.com>
2021-07-07 17:15:32 +0530

    2nd commit - HelloWorld.java

--- src/sample/java/project/HelloWorld.java
+++ src/sample/java/project/HelloWorld.java
@@ -2,6 +2,13 @@ package sample.java.project;
 
 class HelloWorld{  
     public static void main(String args[]){  
-     System.out.println("Hello World...");  
+     System.out.println("Hello World...");
+     amethod();
+    }
+
+    public static void amethod(){
+	
+	System.out.println("You are really awesome");
+
     }
 }
